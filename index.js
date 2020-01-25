35f921a065ea18367cb601ff85d92d766c6403f9
Abdur26 <abdur2192003@gmail.com>
2020-01-25 17:44:31 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+//var operand_one= 80
+//var operand_two= 50
+
+//console.log(operand_one+ operand_two);
+
+// var original_price = 1000
+// var discounted_price = original_price - 200
+
+// console.log(discounted_price);
+var number_one = "1"
+var number_two = 1
+var number_three = 1
+
+console.log(number_one+number_two+number_three)
+
+var number_one = "1"
+var number_two = 1
+var number_three = 1
+console.log(number_one+number_two - number_three)
+
+var number_one = "1"
+var number_two = 1
+var number_three = 1
+console.log(number_one-number_two+number_three)
+
+var number_one = 1
+var number_two = 1
+var number_three = "abrehman"
+console.log(number_three+number_two-number_one)
+
+var user = "A.Rehman"
+
+console.log("welcome :" + user)
+
+var welcome = "hello i am A.Rehman"
+console.log(welcome)
+
+// var divide = 2 / 2
+// var multiply = 2 * 2
+// var plus  = 2 + 2
+// var minus = 2 - 2
+
+// var answer = plus + minus
+//   console.log (answer)
+
+var input = "79"
+
+var odd = input % 3
+
+console.log(odd)
+
+var num = 5
+
+num--
+console.log(num)
+
+var value = 5
+
+console.log(value++)
+
+console.log(value)
